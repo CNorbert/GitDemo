bcd81d7fac92823372a245366495118f0b2bbad3
Norbert Citrak <admin@Mac-Admin.local>
2015-04-30 10:23:40 +0300

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Norbert Citrak on 30.04.15.
+//  Copyright (c) 2015 Norbert Citrak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Norbert Citrak on 30.04.15.
+//  Copyright (c) 2015 Norbert Citrak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
